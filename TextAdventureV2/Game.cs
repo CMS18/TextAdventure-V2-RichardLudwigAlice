d5d482b0915f9d6f0890d77b8035deac79ba2e75
richard davy <richard_davy@hotmail.com>
2018-09-24 13:05:57 +0200

    Flyttat på puzzlet

--- TextAdventureV2/Game.cs
+++ TextAdventureV2/Game.cs
@@ -398,50 +398,6 @@ namespace TextAdventureV2
             Console.WriteLine("GO ...                   Tries to go a certain direction");
             Console.WriteLine("INSPECT ...              Inspects your inventory.");
             Console.WriteLine("USE ...                  Use an item from your inventory.");            
-        }
-        private void Puzzle()
-        {
-
-            Random random = new Random();
-            Console.WriteLine("Seems like the lock needs a number between 1 and 10 to be unlocked. You have 3 attempts.");
-
-            Console.WriteLine("\nGuess a number between 1 and 10.");
-            int number = random.Next(1, 11);
-            int attempts = 1;
-            int guess = 0;
-
-            while (guess != number || attempts == 3)
-            {
-                Console.Write("\nAttempt number {0}:  ", attempts);
-
-                if (Int32.TryParse(Console.ReadLine(), out guess))
-                {
-                    Console.WriteLine("\nYou picked " + guess);
-
-                    if (guess == number)
-                    {
-                        Console.WriteLine("You opened the lock.");
-                        break;
-                    }
-                    else
-                    {
-                        attempts++;
-
-                        if (guess < number)
-                        {
-                            Console.WriteLine("Seems like you need a bigger number.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Seems like you need a smaller number.");
-                        }
-                    }
-                }
-                    else
-                {
-                    Console.WriteLine("That's not a number.");
-                }
-            }
-        }
+        }      
     }
 }
